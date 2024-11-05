9ab74cf3a594c3d0286b4aa5f9ee024a8c85339c
sahilsingh84 <sahil@go-plus.io>
2024-11-05 13:31:31 +0530

    helloWorld

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hiii Hello <code>src/App.js</code> and save to reload.
+          Hiii Toh Kaise hai Aap Log Hello <code>src/App.js</code> and save to
+          reload.
         </p>
         <a
           className="App-link"
